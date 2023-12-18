83a7bc7cb35602518195d4c016d0b08ba0c498a2
Mukul-Vyas <mukulvyas2001@gmail.com>
2023-12-18 17:28:09 +0530

    login fixed

--- src/components/userLogin.js
+++ src/components/userLogin.js
@@ -9,7 +9,8 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-datepicker/dist/react-datepicker.min.css";
 
-export default function UserLogin() {
+export default function UserLogin()
+{
   let isTab = useMediaQuery({ query: "(max-width: 640px)" });
   const navigate = useNavigate();
   const baseUrl = process.env.REACT_APP_BASE_URL;
@@ -22,41 +23,61 @@ export default function UserLogin() {
   const [error, setError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [isValid, setIsValid] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
+  const [checkboxerror, setCheckboxerror] = useState();
   const [isValidNumber, setIsValidNumber] = useState(true);
 
-  const handleMobileNumberChange = (e) => {
+
+  const handleCheckboxChange = (event) =>
+  {
+    setIsChecked(event.target.checked);
+  };
+
+  const handleMobileNumberChange = (e) =>
+  {
+
     const number = e.target.value;
     const isValidNumber = validateMobileNumber(number);
+    setIsValid(isValidNumber);
+    setContactNumber(number);
     setIsValidNumber(isValidNumber);
-    if (isValidNumber || number === "") {
+    if (isValidNumber || number === "")
+    {
       setContactNumber(number);
     }
   };
 
-  const validateMobileNumber = (number) => {
+  const validateMobileNumber = (number) =>
+  {
+
     const isValidFormat = /^\d{0,10}$/.test(number); // Validates up to 10 digits
     return isValidFormat;
   };
-  const handlePasswordChange = (e) => {
+  const handlePasswordChange = (e) =>
+  {
     const enteredPassword = e.target.value;
     setPassword(enteredPassword);
 
     // Password validation
-    if (enteredPassword.trim().length < 6) {
+    if (enteredPassword.trim().length < 6)
+    {
       setPasswordError("Password should be at least 6 characters");
-    } else {
+    } else
+    {
       setPasswordError("");
     }
   };
 
-  const handleForgetPassword = async (e) => {
+  const handleForgetPassword = async (e) =>
+  {
     e.preventDefault();
     const requestBody = {
       contactNumber: contactNumber,
     };
     const apiUrl = `${baseUrl}/api/v1/user/send_otp`;
 
-    try {
+    try
+    {
       // Send the POST request
       const response = await fetch(apiUrl, {
         method: "POST",
@@ -71,27 +92,38 @@ export default function UserLogin() {
       const data = await response.json();
 
       // Check the response status
-      if (response.ok) {
+      if (response.ok)
+      {
         console.log("OTP sent successfully", data);
         localStorage.setItem("contactNumber", contactNumber);
         navigate("/otpverify");
-      } else {
+      } else
+      {
         console.error("Error sending OTP:", data);
       }
-    } catch (error) {
+    } catch (error)
+    {
       console.error("Error during the API call:", error);
     }
   };
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     const doctor = location?.state;
     setSelectedDoctor(doctor?.doctor);
     console.log("SELECTED DOCTOR", selectedDoctor);
   }, []);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e) =>
+  {
     e.preventDefault();
-    if (isDoctor) {
+
+    if (!isChecked)
+    {
+      setCheckboxerror("Please select the Checkbox")
+    }
+    if (isValid && isChecked)
+    {
       const response = await fetch(`${baseUrl}/api/v1/user/send_otp`, {
         method: "post",
         headers: {
@@ -103,11 +135,13 @@ export default function UserLogin() {
       });
       const data = await response.json();
       console.log("DATA FROM RESPONSE", data);
-      if (data?.user?._id) {
+      if (data?.user?._id)
+      {
         localStorage.setItem("userId", data?.user?._id);
         localStorage.setItem("name", data?.user?.name);
         localStorage.setItem("userContactNumber", data?.user?.email);
-      } else {
+      } else
+      {
         localStorage.setItem("userId", data?.data?._id);
       }
       localStorage.setItem("contactNumber", contactNumber);
@@ -131,29 +165,33 @@ export default function UserLogin() {
       </div>
       <div className="right_side">
         <h3 className="right_heading">User Login</h3>
-        <p className="para_one">Add Your Mobile Number</p>
-        <p className="para_two">Apply with your phone number</p>
+        <p className="para_one" style={{ fontSize: '22px', fontWeight: 700 }}>Add Your Mobile Number</p>
+        <p className="para_two" style={{ fontSize: '18px', fontWeight: 300 }}>Apply with your phone number</p>
         <div className="input_container">
           <FaPhoneAlt className="call_icon" />
           <input
-            className={`input_box ${isValidNumber ? "" : "invalid"}`}
+            className={`input_box ${isValid ? "" : "invalid"}`}
             type="text"
             placeholder="Enter your mobile number"
             value={contactNumber}
             onChange={handleMobileNumberChange}
+            maxLength={10}
           />
-          {!isValidNumber && (
-            <p className="error_message">Please enter correct number.</p>
-          )}
-        </div>{" "}
-        <label className="label1">
-          <input type="checkbox" /> I agree with the{" "}
-          <a href="#terms" style={{ color: "#666", fontWeight: "600" }}>
-            terms of service
+          {!isValid && (
+            <p className="error_message">Please enter a valid mobile number</p>
+          )}{isValid && !isChecked && (
+            <p className="error_message">{checkboxerror}</p>
+          )}{" "}
+        </div>
+        <label className="label1" style={{ fontWeight: 400, fontSize: '16px' }}>
+          <input type="checkbox" checked={isChecked}
+            onChange={handleCheckboxChange} /> I agree with the{" "}
+          <a onClick={() => navigate("/termsofservices")} style={{ color: "#666", fontWeight: 'bolder', textDecoration: 'underline' }}>
+            Terms of service
           </a>{" "}
           and{" "}
-          <a href="#privacy" style={{ color: "#666", fontWeight: "600" }}>
-            privacy policy
+          <a onClick={() => navigate("/privacypolicy")} style={{ color: "#666", fontWeight: 'bolder', textDecoration: 'underline' }}>
+            Privacy policy
           </a>
         </label>
         <button className="button1" onClick={handleSubmit}>
