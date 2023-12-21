a5da0b74f8d0052c0eb62dac702f92e32640935f
Mukul-Vyas <mukulvyas2001@gmail.com>
2023-12-21 16:55:44 +0530

    nw

--- src/components/testlistpage.js
+++ src/components/testlistpage.js
@@ -35,6 +35,8 @@ export default function TestListPage()
     const [data, setData] = useState([
         { id: 1, test: 'Viral Load', testCode: 'Hiv', department: 'Molucular Test', sampleType: 'serum', cost: '$50', isEditing: false },
         { id: 2, test: 'Vitamin D3', testCode: 'vitamin d3', department: 'Immuinio', sampleType: 'serum', cost: '$75', isEditing: false },
+        { id: 3, test: 'Viral Load', testCode: 'Hiv', department: 'Molucular Test', sampleType: 'serum', cost: '$50', isEditing: false },
+        { id: 4, test: 'Vitamin D3', testCode: 'vitamin d3', department: 'Immuinio', sampleType: 'serum', cost: '$75', isEditing: false },
     ]);
 
     const addRow = () =>
