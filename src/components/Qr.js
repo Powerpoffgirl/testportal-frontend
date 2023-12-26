a3e21648a6b2242adf5d2341abca6e1f77de8c00
ramkumar21234 <126386524+RaM21234@users.noreply.github.com>
2023-12-26 17:15:17 +0530

    co

--- src/components/Qr.js
+++ src/components/Qr.js
@@ -3,24 +3,46 @@ import Sidebar from "./sidebar";
 import Header from "./header";
 import { useMediaQuery } from "react-responsive";
 import AdminSidebar from "./adminSidebar";
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
 
-export default function Qr()
-{
+export default function Qr() {
   let isTab = useMediaQuery({ query: "(max-width: 768px)" });
   const baseUrl = process.env.REACT_APP_BASE_URL
   const [qrCodeUrl, setQrCodeUrl] = useState("");
   const [name, setName] = useState("")
+  const [userDetails, setUserDetails] = useState(null);
 
-  useEffect(() =>
-  {
-    const getQRCode = async () =>
-    {
-      try
-      {
+  const id = localStorage.getItem('id')
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const getQRCode = async () => {
+
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`${baseUrl}/api/v1/admin/get_doctor/${id}`, {
+            method: "GET",
+            headers: {
+              "x-auth-token": token,
+            },
+            // body: JSON.stringify(doctorDetails),
+          });
+          const data = await response.json();
+          setUserDetails(data.data);
+          console.log("registered data", userDetails)
+          console.log("address", userDetails.address.houseNo)
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
+      fetchData();
+
+
+      try {
         const token = localStorage.getItem("token");
         const id = localStorage.getItem("id")
-        if (!token)
-        {
+        if (!token) {
           console.error("No token found in local storage");
           return;
         }
@@ -38,16 +60,14 @@ export default function Qr()
         console.log("DOCTOR DETAILS", doctorDetails.qrCodeUrl)
         setQrCodeUrl(doctorDetails.qrCodeUrl)
         setName(doctorDetails.name)
-      } catch (error)
-      {
+      } catch (error) {
         console.error('There was an error verifying the OTP:', error);
       }
     }
     getQRCode()
   }, [])
 
-  const handleDownload = () =>
-  {
+  const handleDownload = () => {
     // Create a new anchor element
     const element = document.createElement("a");
     element.href = `${qrCodeUrl}`;
@@ -65,14 +85,161 @@ export default function Qr()
       >
 
         <div
-          className="flex flex-col bg-customGreen"
+          className="flex flex-col bg-customGreen "
           style={{
             width: isTab ? "100%" : "77%",
           }}
         >
 
-          <div
-            className="flex flex-col gap-2 px-3 w-full"
+
+          <div className="flex flex-row">
+            {/* --------------left-------------- */}
+            <div className="flex flex-col border bg-white w-1/2 p-6 m-5">
+
+
+
+              <div className="mx-auto my-2">
+                <div className=" " >
+                  <div className=" border w-36 mx-auto rounded-full" style={{ backgroundColor: '#B1DAED' }}>
+                    {userDetails?.doctorPic ? (
+                      <img
+                        src={userDetails?.doctorPic}
+                        alt="Avatar"
+                        style={{
+                          borderRadius: "50%",
+                          width: '130px',
+                          height: '130px'
+                        }}
+                      />
+                    ) : (
+                      <PermIdentityOutlinedIcon
+                        style={{ width: "auto", height: 'auto', color: 'white' }}
+                      />
+                    )}
+                  </div>
+                </div>
+
+
+              </div>
+
+              {/* -------name------- */}
+              <div className="mt-3 flex flex-row">
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Name :
+                </p>
+                <p className="block text-black text-lg "> {userDetails?.name}</p>
+              </div>
+
+              {/* ------------email------------ */}
+              <div className="mt-3 flex flex-row">
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Email :
+                </p>
+                <p className="block text-black text-lg "> {userDetails?.email}</p>
+              </div>
+              {/* -----------contact----------- */}
+              <div className="mt-3 flex flex-row">
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Mobile Number :
+                </p>
+                <p className="block text-black text-lg "> {userDetails?.contactNumber}</p>
+              </div>
+
+              <div className="mt-3 flex flex-row">
+                <p className="block text-black text-lg font-semibold" >Working Days :</p>
+                <p className=" text-black text-lg flex flex-wrap">
+                  {userDetails?.workingDays.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        {item},
+                      </div>
+                    )
+                  })}
+                </p>
+
+              </div>
+
+              <div className="mt-3 flex flex-row">
+                <p className="block text-black text-lg font-semibold">Working Hours :</p>
+                <p className="block text-black text-lg ">
+                  {userDetails?.workingHours?.workHourFrom} - {userDetails?.workingHours?.workHourTo}
+
+                </p>
+
+              </div>
+
+              <div className=" mt-3 flex flex-row">
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Total Experience :
+                </p>
+                <p className="block text-black text-lg ">{userDetails?.totalExperience}</p>
+              </div>
+
+              <div className="mt-3 flex flex-row" >
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Specialist:
+                </p>
+                <p className="block text-black text-lg ">{userDetails?.speciality}</p>
+              </div>
+
+              <div className="mt-3 flex flex-row">
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Degree:
+                </p>
+
+                <p className="block text-black text-lg ">{userDetails?.degree}</p>
+              </div>
+
+              {/* -----------address----------- */}
+              <div className="mt-3 flex flex-row ">
+                <p
+                  className="block text-black text-lg font-semibold"
+                >
+                  Address :
+                </p>
+                <p className=" text-black text-lg flex " >{userDetails?.address?.houseNo}, {userDetails?.address?.floor}, {userDetails?.address?.block}, {userDetails?.address?.pinCode}, {userDetails?.address?.area} ,{userDetails?.address?.district} ,{userDetails?.address?.state}</p>
+              </div>
+            </div>
+
+            {/* ----------------------------------right---------------------------------- */}
+            <div className="border bg-white flex flex-col w-1/2 p-6 my-5 mx-3">
+              <text
+                className="text-center"
+                style={{
+                  fontWeight: 600,
+                  fontSize: "24px",
+                  lineHeight: "28.8px",
+                  fontFamily: "Lato, sans-serif",
+                }}
+              >
+                Generate Personal QR
+              </text>
+              <p class="mx-auto ">
+                {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}
+              </p>
+
+
+              <div className="flex mx-auto mt-5 my-2">
+                <button className="btn btn-primary border py-3 px-4 rounded-3xl text-white" style={{ backgroundColor: '#89CFF0' }} onClick={handleDownload}>                   Download
+                </button>
+              </div>
+            </div>
+
+
+            {/* <div
+            className="flex flex-col gap-2 px-3 w-full border"
             style={{
               top: "4%",
               left: "2%",
@@ -115,8 +282,9 @@ export default function Qr()
                 Download
               </button>
             </div>
+          </div> */}
           </div>
-        </div>
+        </div >
       </div >
     </>
   );
