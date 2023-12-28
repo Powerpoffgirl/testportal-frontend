1613f7073d27bc029b03ce738851bf10a0cb116e
cyberne7work <cyberne7work@gmail.com>
2023-12-28 15:11:42 +0530

    new code added

--- src/components/doctorList.js
+++ src/components/doctorList.js
@@ -42,7 +42,8 @@ const svg5 = `<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns=
 <path d="M4.6875 24.9999C3.82812 24.9999 3.09245 24.7279 2.48047 24.1839C1.86849 23.6399 1.5625 22.986 1.5625 22.2221V4.16654H0V1.38877H7.8125V-0.00012207H17.1875V1.38877H25V4.16654H23.4375V22.2221C23.4375 22.986 23.1315 23.6399 22.5195 24.1839C21.9076 24.7279 21.1719 24.9999 20.3125 24.9999H4.6875ZM20.3125 4.16654H4.6875V22.2221H20.3125V4.16654ZM7.8125 19.4443H10.9375V6.94432H7.8125V19.4443ZM14.0625 19.4443H17.1875V6.94432H14.0625V19.4443Z" fill="white"/>
 </svg>`;
 
-export default function DoctorList({ searchTerm }) {
+export default function DoctorList({ searchTerm })
+{
   let isTab = useMediaQuery({ query: "(max-width: 768px)" });
   const [doctorsList, setDoctorsList] = useState([]);
   const baseUrl = process.env.REACT_APP_BASE_URL;
@@ -62,17 +63,18 @@ export default function DoctorList({ searchTerm }) {
     "Urology",
   ];
 
-  useEffect(() => {
-    localStorage.clear();
-  }, []);
   useEffect(() =>
   {
-    localStorage.clear()
-  }, [])
+    localStorage.clear();
+  }, []);
 
-  useEffect(() => {
-    const fetchDoctorDetails = async () => {
-      try {
+
+  useEffect(() =>
+  {
+    const fetchDoctorDetails = async () =>
+    {
+      try
+      {
         const response = await fetch(`${baseUrl}/api/v1/list_doctors`, {
           method: "GET",
           headers: {
@@ -86,29 +88,22 @@ export default function DoctorList({ searchTerm }) {
           (doctor) => doctor.accountVerified.isVerified
         );
         setDoctorsList(verifiedDoctors);
-<<<<<<< HEAD
       } catch (error)
       {
-=======
-      } catch (error) {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
+
         console.error("There was an error verifying the OTP:", error);
       }
     };
     fetchDoctorDetails();
   }, [searchTerm]);
 
-<<<<<<< HEAD
+
   useEffect(() =>
   {
     // Check if there is a searchTerm and the doctorsList is not empty.
+
     if (doctorsList.length > 0 && searchTerm)
     {
-=======
-  useEffect(() => {
-    // Check if there is a searchTerm and the doctorsList is not empty.
-    if (doctorsList.length > 0 && searchTerm) {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
       const lowerCaseSearchTerm = searchTerm.toLowerCase().trim();
       const matchedDoctors = doctorsList.filter(
         (doctor) =>
@@ -116,23 +111,17 @@ export default function DoctorList({ searchTerm }) {
           doctor.speciality.toLowerCase().includes(lowerCaseSearchTerm)
       );
       setFilteredDoctors(matchedDoctors);
-<<<<<<< HEAD
     } else
     {
-=======
-    } else {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
+
       // If no searchTerm or doctorsList is empty, use the original list.
       setFilteredDoctors(doctorsList);
     }
   }, [doctorsList, searchTerm]); // Include all dependencies in the dependency array
 
-<<<<<<< HEAD
+
   const handleQRCode = (doctorId) =>
   {
-=======
-  const handleQRCode = (doctorId) => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
     console.log("HELLO");
     localStorage.setItem("doctorId", doctorId);
     const doctor = doctorsList?.find((doc) => doc._id === doctorId);
@@ -143,13 +132,9 @@ export default function DoctorList({ searchTerm }) {
     onOpenModal();
   };
 
-<<<<<<< HEAD
   const handleBookAppointment = async () =>
   {
-=======
-  const handleBookAppointment = async () => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-    console.log(selectedDoctor?.slots[currentIndex]);
+    console.log(selectedDoctor?.slots[currentIndex])
     const bookslot = {
       date: selectedDoctor?.slots[currentIndex].date.split("T")[0],
       time: selectedDoctor?.slots[currentIndex].startTime,
@@ -183,11 +168,14 @@ export default function DoctorList({ searchTerm }) {
 
   };
 
-  const handleFilterDocotors = (item) => {
+  const handleFilterDocotors = (item) =>
+  {
     console.log("ITEM NAME IS================>", item);
-    if (item.toLowerCase() === "all") {
+    if (item.toLowerCase() === "all")
+    {
       setFilteredDoctors(doctorsList);
-    } else {
+    } else
+    {
       const filteredDoctors = doctorsList.filter(
         (doc) => doc.speciality === item
       );
@@ -205,32 +193,35 @@ export default function DoctorList({ searchTerm }) {
   const MAX_LENGTH = 6;
   const otpInputs = [];
 
-<<<<<<< HEAD
   const handleChange = (e) =>
   {
-=======
-  const handleChange = (e) => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-    let { name, value } = e.target;
-    console.log(value);
-    setcontactNumber(value);
-    console.log(contactNumber);
-  };
+    let { name, value } = e.target
+    console.log(value)
+    if (value.length != 10)
+    {
+      setmobileNumberError('Please enter a valid number')
+    }
+    if (value.length == 10)
+    {
+      setmobileNumberError('')
+    }
+    setcontactNumber(value)
+    console.log(contactNumber)
+  }
 
-  const showSlot = () => {
-    setbookingslottoggle(!bookingslottoggle);
-  };
+  const showSlot = () =>
+  {
+    setbookingslottoggle(!bookingslottoggle)
+  }
 
-  const showappointment = () => {
-    setappointment(!appointment);
-  };
+  const showappointment = () =>
+  {
 
-<<<<<<< HEAD
+    setappointment(!appointment)
+  }
+
   const handleOtp = async () =>
   {
-=======
-  const handleOtp = async () => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
     const response = await fetch(`${baseUrl}/api/v1/user/send_otp`, {
       method: "post",
       headers: {
@@ -241,46 +232,34 @@ export default function DoctorList({ searchTerm }) {
     const data = await response.json();
     console.log("RESPONSE------", data);
     console.log("user id", data?.user?._id);
-    localStorage.setItem("userId", data?.data?._id);
+    localStorage.setItem("userId", data?.user?._id);
 
     // localStorage.setItem("token", data?.user?.token)
     setotppage(true)
 
   }
-<<<<<<< HEAD
   const handleInputChange = (e, index) =>
   {
-=======
-  const handleInputChange = (e, index) => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
     const value = e.target.value;
 
-    if (isNaN(value)) {
+    if (isNaN(value))
+    {
       return; // Allow only numeric input
     }
 
     otp[index] = value;
-<<<<<<< HEAD
+
     if (index < MAX_LENGTH - 1 && value)
     {
-=======
-    if (index < MAX_LENGTH - 1 && value) {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
       otpInputs[index + 1].focus();
     }
 
     setOtp([...otp]);
-  };
-
-<<<<<<< HEAD
+  }
   const verifyOTP = async () =>
   {
     try
     {
-=======
-  const verifyOTP = async () => {
-    try {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
       const userId = localStorage.getItem("userId")
 
       const otpString = otp.join("");
@@ -297,24 +276,17 @@ export default function DoctorList({ searchTerm }) {
       );
 
       const data = await response.json();
-<<<<<<< HEAD
       if (data.success === true)
       {
-=======
-      if (data.success === true) {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-        console.log("DATA from response", data);
+        console.log("DATA from response", data)
 
         localStorage.setItem("token", data?.data?.token);
         navigate("/edituserform");
       }
-<<<<<<< HEAD
+
     } catch (error)
     {
-=======
-    } catch (error) {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-      console.error("There was an error verifying the OTP:", error);
+      console.error('There was an error verifying the OTP:', error);
     }
   };
 
@@ -323,7 +295,8 @@ export default function DoctorList({ searchTerm }) {
   const numberOfColumns = 4;
   const numberOfRows = Math.ceil(bookingslot?.length / numberOfColumns);
 
-  function getYearMonthDay(dateString) {
+  function getYearMonthDay(dateString)
+  {
     // Create a new Date object using the provided date string
     const date = new Date(dateString);
 
@@ -355,23 +328,22 @@ export default function DoctorList({ searchTerm }) {
     return { year, monthName, day, dayName };
   }
 
-<<<<<<< HEAD
+
   const handleDateClick = (index) =>
   {
-=======
-  const handleDateClick = (index) => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
     setCurrentIndex(index);
   };
 
-  const goToNext = () => {
+  const goToNext = () =>
+  {
     const isLastItem = currentIndex === bookingslot.length - 1;
     const nextIndex = isLastItem ? 0 : currentIndex + 1;
     setCurrentIndex(nextIndex);
     console.log(currentIndex);
   };
 
-  const goToPrev = () => {
+  const goToPrev = () =>
+  {
     const isFirstItem = currentIndex === 0;
     const prevIndex = isFirstItem ? bookingslot.length - 1 : currentIndex - 1;
     setCurrentIndex(prevIndex);
@@ -460,7 +432,8 @@ export default function DoctorList({ searchTerm }) {
               <div className=" py-1 mb-2">
                 <p className="text-lg font-medium text-black " >SPECIALITY</p>
                 <div className="flex flex-wrap ">
-                  {selectedDoctor?.speciality?.map((item, index) => {
+                  {selectedDoctor?.speciality?.map((item, index) =>
+                  {
                     return (
                       <p key={index} className="bg-white rounded-xl py-1 px-4 mx-2 my-1 ">{item}</p>
                     )
@@ -540,70 +513,52 @@ export default function DoctorList({ searchTerm }) {
                                 <FaAngleLeft style={{ color: 'black' }} />
                               </button>
                               <div className="flex flex-row overflow-x-auto mx-2 ">
+                                {bookingslot?.map((data, index) =>
                                 {
-<<<<<<< HEAD
-                                  bookingslot?.map((data, index) =>
+                                  console.log(data)
+                                  const { year, monthName, day, dayName } = getYearMonthDay(data.date)
+                                  // console.log(year, monthName, day, dayName)
+                                  if (data.isBooked == true)
                                   {
-=======
-                                  bookingslot?.map((data, index) => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-                                    // console.log(data.date)
-                                    const { year, monthName, day, dayName } =
-                                      getYearMonthDay(data.date);
-                                    // console.log(year, monthName, day, dayName)
-<<<<<<< HEAD
-                                    if (index == currentIndex)
-                                    {
-=======
-                                    if (index == currentIndex) {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-                                      return (
-                                        <div key={index} className="flex flex-col px-2" disabled >
-                                          <p>{monthName}</p>
-                                          <p className=" p-2 border-2 rounded-lg " style={{ backgroundColor: '#89CFF0' }}>{day}</p>
-                                          <p>{dayName}</p>
-                                        </div>
-                                      );
-<<<<<<< HEAD
-                                    } else
-                                    {
-=======
-                                    } else {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-                                      return (
-                                        <div
-                                          key={index}
-                                          className="flex flex-col px-2 hover:cursor-pointer"
-<<<<<<< HEAD
-                                          onClick={() =>
-                                          {
-=======
-                                          onClick={() => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
-                                            handleDateClick(index);
-                                          }}
-                                        >
-                                          <p>{monthName}</p>
-                                          <p className=" p-2 border-2 rounded-lg bg-gray-200" >{day}</p>
-                                          <p>{dayName}</p>
-                                        </div>
-                                      )
-                                    }
-                                  })
-                                }
-                              </div >
+                                    return (
+                                      <div key={index} className="flex flex-col px-2" disabled >
+                                        <p>{monthName}</p>
+                                        <p className=" p-2 border-2 rounded-lg " style={{ backgroundColor: '#89CFF0' }}>{day}</p>
+                                        <p>{dayName}</p>
+                                      </div>
+                                    )
+                                  }
+                                  else if (index == currentIndex)
+                                  {
+                                    return (
+                                      <div key={index} className="flex flex-col px-2"
+                                      >
+                                        <p>{monthName}</p>
+                                        <p className=" p-2 border-2 rounded-lg bg-blue-100" >{day}</p>
+                                        <p>{dayName}</p>
+                                      </div>
+                                    )
+                                  } else
+                                  {
+                                    return (
+                                      <div key={index} className="flex flex-col px-2 hover:cursor-pointer" onClick={() => { handleDateClick(index) }}
+                                      >
+                                        <p>{monthName}</p>
+                                        <p className=" p-2 border-2 rounded-lg bg-gray-200" >{day}</p>
+                                        <p>{dayName}</p>
+                                      </div>
+                                    )
+                                  }
+                                })}
+                              </div>
                               <button className="text-white text-xs rounded-3xl" onClick={goToNext} >
                                 <FaAngleRight style={{ color: 'black' }} />
                               </button>
-                            </div >
+                            </div>
 
                             <div className="flex flex-col space-y-2 my-2 overflow-y-scroll h-32 px-2">
-<<<<<<< HEAD
                               {[...Array(numberOfRows)].map((_, rowIndex) =>
                               {
-=======
-                              {[...Array(numberOfRows)].map((_, rowIndex) => {
->>>>>>> 58821150d0d490dc06d5962a36d88c284845eeed
                                 return (
                                   <div key={rowIndex} className="flex space-x-2">
                                     {bookingslot.slice(rowIndex * numberOfColumns, (rowIndex + 1) * numberOfColumns)?.map((data, index) =>
@@ -658,11 +613,11 @@ export default function DoctorList({ searchTerm }) {
                         </button> */}
                         </div>}
                       </div>
-                    </div >
-                  </div >
-                </div >
-              </div >
-            </div >
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             }
             {otppage &&
               <div className="border bg-white flex flex-col md:w-1/2  p-4  mx-1">
@@ -746,7 +701,7 @@ export default function DoctorList({ searchTerm }) {
                 </div>
               </div>}
           </div >
-        </div >
+        </div>
       </Modal >
 
 
