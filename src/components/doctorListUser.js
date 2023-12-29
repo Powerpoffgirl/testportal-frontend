f0a7b5e94706e7e1f3e1e1786562840183b758b2
cyberne7work <cyberne7work@gmail.com>
2023-12-29 12:56:02 +0530

    co

--- src/components/doctorListUser.js
+++ src/components/doctorListUser.js
@@ -35,7 +35,8 @@ const svg5 = `<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns=
 <path d="M4.6875 24.9999C3.82812 24.9999 3.09245 24.7279 2.48047 24.1839C1.86849 23.6399 1.5625 22.986 1.5625 22.2221V4.16654H0V1.38877H7.8125V-0.00012207H17.1875V1.38877H25V4.16654H23.4375V22.2221C23.4375 22.986 23.1315 23.6399 22.5195 24.1839C21.9076 24.7279 21.1719 24.9999 20.3125 24.9999H4.6875ZM20.3125 4.16654H4.6875V22.2221H20.3125V4.16654ZM7.8125 19.4443H10.9375V6.94432H7.8125V19.4443ZM14.0625 19.4443H17.1875V6.94432H14.0625V19.4443Z" fill="white"/>
 </svg>`;
 
-export default function DoctorListUser({ searchTerm }) {
+export default function DoctorListUser({ searchTerm })
+{
   let isTab = useMediaQuery({ query: "(max-width: 768px)" });
   const [doctorsList, setDoctorsList] = useState([]);
   const baseUrl = process.env.REACT_APP_BASE_URL;
@@ -62,9 +63,12 @@ export default function DoctorListUser({ searchTerm }) {
   //   localStorage.clear();
   // }, []);
 
-  useEffect(() => {
-    const fetchDoctorDetails = async () => {
-      try {
+  useEffect(() =>
+  {
+    const fetchDoctorDetails = async () =>
+    {
+      try
+      {
         const response = await fetch(`${baseUrl}/api/v1/list_doctors`, {
           method: "GET",
           headers: {
@@ -78,16 +82,19 @@ export default function DoctorListUser({ searchTerm }) {
           (doctor) => doctor.accountVerified.isVerified
         );
         setDoctorsList(verifiedDoctors);
-      } catch (error) {
+      } catch (error)
+      {
         console.error("There was an error verifying the OTP:", error);
       }
     };
     fetchDoctorDetails();
   }, [searchTerm]);
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     // Check if there is a searchTerm and the doctorsList is not empty.
-    if (doctorsList.length > 0 && searchTerm) {
+    if (doctorsList.length > 0 && searchTerm)
+    {
       const lowerCaseSearchTerm = searchTerm.toLowerCase().trim();
       const matchedDoctors = doctorsList.filter(
         (doctor) =>
@@ -95,13 +102,15 @@ export default function DoctorListUser({ searchTerm }) {
           doctor.speciality.toLowerCase().includes(lowerCaseSearchTerm)
       );
       setFilteredDoctors(matchedDoctors);
-    } else {
+    } else
+    {
       // If no searchTerm or doctorsList is empty, use the original list.
       setFilteredDoctors(doctorsList);
     }
   }, [doctorsList, searchTerm]); // Include all dependencies in the dependency array
 
-  const handleQRCode = (doctorId) => {
+  const handleQRCode = (doctorId) =>
+  {
     console.log("HELLO");
     localStorage.setItem("doctorId", doctorId);
     const doctor = doctorsList?.find((doc) => doc._id === doctorId);
@@ -112,7 +121,8 @@ export default function DoctorListUser({ searchTerm }) {
     onOpenModal();
   };
 
-  const handleBookAppointment = async () => {
+  const handleBookAppointment = async () =>
+  {
     console.log(selectedDoctor?.slots[currentIndex]);
     const bookslot = {
       date: selectedDoctor?.slots[currentIndex].date.split("T")[0],
@@ -133,12 +143,8 @@ export default function DoctorListUser({ searchTerm }) {
     const data = await response.json();
 
     console.log("slot booked", data);
-<<<<<<< HEAD
-    if (data.success === true) {
-=======
     if (data.success === true)
     {
->>>>>>> 83beffd2c3736bde104310bedabf33a8333b9d79
       navigate("/edituserform");
     }
     localStorage.setItem(
@@ -151,11 +157,14 @@ export default function DoctorListUser({ searchTerm }) {
     showSlot();
   };
 
-  const handleFilterDocotors = (item) => {
+  const handleFilterDocotors = (item) =>
+  {
     console.log("ITEM NAME IS================>", item);
-    if (item.toLowerCase() === "all") {
+    if (item.toLowerCase() === "all")
+    {
       setFilteredDoctors(doctorsList);
-    } else {
+    } else
+    {
       const filteredDoctors = doctorsList.filter(
         (doc) => doc.speciality === item
       );
@@ -174,22 +183,26 @@ export default function DoctorListUser({ searchTerm }) {
   const MAX_LENGTH = 6;
   const otpInputs = [];
 
-  const handleChange = (e) => {
+  const handleChange = (e) =>
+  {
     let { name, value } = e.target;
     console.log(value);
     setcontactNumber(value);
     console.log(contactNumber);
   };
 
-  const showSlot = () => {
+  const showSlot = () =>
+  {
     setbookingslottoggle(!bookingslottoggle);
   };
 
-  const showappointment = () => {
+  const showappointment = () =>
+  {
     setappointment(!appointment);
   };
 
-  const handleOtp = async () => {
+  const handleOtp = async () =>
+  {
     const response = await fetch(`${baseUrl}/api/v1/user/send_otp`, {
       method: "post",
       headers: {
@@ -205,24 +218,29 @@ export default function DoctorListUser({ searchTerm }) {
     // localStorage.setItem("token", data?.user?.token)
     setotppage(true);
   };
-  const handleInputChange = (e, index) => {
+  const handleInputChange = (e, index) =>
+  {
     const value = e.target.value;
 
-    if (isNaN(value)) {
+    if (isNaN(value))
+    {
       return; // Allow only numeric input
     }
 
     otp[index] = value;
 
-    if (index < MAX_LENGTH - 1 && value) {
+    if (index < MAX_LENGTH - 1 && value)
+    {
       otpInputs[index + 1].focus();
     }
 
     setOtp([...otp]);
   };
 
-  const verifyOTP = async () => {
-    try {
+  const verifyOTP = async () =>
+  {
+    try
+    {
       const userId = localStorage.getItem("userId");
 
       const otpString = otp.join("");
@@ -239,13 +257,15 @@ export default function DoctorListUser({ searchTerm }) {
       );
 
       const data = await response.json();
-      if (data.success === true) {
+      if (data.success === true)
+      {
         console.log("DATA from response", data);
 
         localStorage.setItem("token", data?.data?.token);
         navigate("/edituserform");
       }
-    } catch (error) {
+    } catch (error)
+    {
       console.error("There was an error verifying the OTP:", error);
     }
   };
@@ -255,7 +275,8 @@ export default function DoctorListUser({ searchTerm }) {
   const numberOfColumns = 4;
   const numberOfRows = Math.ceil(bookingslot?.length / numberOfColumns);
 
-  function getYearMonthDay(dateString) {
+  function getYearMonthDay(dateString)
+  {
     // Create a new Date object using the provided date string
     const date = new Date(dateString);
 
@@ -287,18 +308,21 @@ export default function DoctorListUser({ searchTerm }) {
     return { year, monthName, day, dayName };
   }
 
-  const handleDateClick = (index) => {
+  const handleDateClick = (index) =>
+  {
     setCurrentIndex(index);
   };
 
-  const goToNext = () => {
+  const goToNext = () =>
+  {
     const isLastItem = currentIndex === bookingslot.length - 1;
     const nextIndex = isLastItem ? 0 : currentIndex + 1;
     setCurrentIndex(nextIndex);
     console.log(currentIndex);
   };
 
-  const goToPrev = () => {
+  const goToPrev = () =>
+  {
     const isFirstItem = currentIndex === 0;
     const prevIndex = isFirstItem ? bookingslot.length - 1 : currentIndex - 1;
     setCurrentIndex(prevIndex);
@@ -715,7 +739,8 @@ export default function DoctorListUser({ searchTerm }) {
                         {!bookingslottoggle && !appointment && (
                           <button
                             className="text-white text-xs rounded-3xl px-3 py-1 "
-                            onClick={() => {
+                            onClick={() =>
+                            {
                               showSlot();
                             }}
                             style={{ backgroundColor: " #89CFF0" }}
