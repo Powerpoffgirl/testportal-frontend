eabcc83b97696c9d11c0c1ea58c3badb36f8b804
Mukul-Vyas <mukulvyas2001@gmail.com>
2023-12-29 17:23:37 +0530

    functional

--- src/components/table.js
+++ src/components/table.js
@@ -5,49 +5,51 @@ import "./Table.css";
 export const Table = ({ rows, deleteRow, editRow }) =>
 {
     return (
-        <div className="table-wrapper">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th className="expand">Test</th>
-                        <th className="expand">testCode</th>
-                        <th className="expand">department</th>
-                        <th className="expand">sampleType</th>
-                        <th className="expand">cost</th>
-                        <th className="expand">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows.map((row, idx) =>
-                    {
-                        const statusText = row.status
-                            ? row.status.charAt(0).toUpperCase() + row.status.slice(1)
-                            : '';
+        <div>
+            <div className="table-wrapper" style={{ height: 420 }}>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th className="expand">Test</th>
+                            <th className="expand">TestCode</th>
+                            <th className="expand">Department</th>
+                            <th className="expand">SampleType</th>
+                            <th className="expand">Cost</th>
+                            <th className="expand">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows.map((row, idx) =>
+                        {
+                            const statusText = row.status
+                                ? row.status.charAt(0).toUpperCase() + row.status.slice(1)
+                                : '';
 
-                        return (
-                            <tr key={idx}>
-                                <td>{row.testName}</td>
-                                <td>{row.testCode}</td>
-                                <td>{row.department}</td>
-                                <td>{row.sampleType}</td>
-                                <td>{row.costOfDiagnosticTest}</td>
-                                <td className="fit">
-                                    <span className="actions">
-                                        <BsFillTrashFill
-                                            className="delete-btn"
-                                            onClick={() => deleteRow(idx)}
-                                        />
-                                        <BsFillPencilFill
-                                            className="edit-btn"
-                                            onClick={() => editRow(idx)}
-                                        />
-                                    </span>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </div >
+                            return (
+                                <tr key={idx}>
+                                    <td style={{ textAlign: 'center' }}>{row.testName}</td>
+                                    <td style={{ textAlign: 'center' }}>{row.testCode}</td>
+                                    <td style={{ textAlign: 'center' }}>{row.department}</td>
+                                    <td style={{ textAlign: 'center' }}>{row.sampleType}</td>
+                                    <td style={{ textAlign: 'center' }}>{row.costOfDiagnosticTest}</td>
+                                    <td className="fit">
+                                        <span className="actions">
+                                            <BsFillTrashFill
+                                                className="delete-btn"
+                                                onClick={() => deleteRow(idx)}
+                                            />
+                                            <BsFillPencilFill
+                                                className="edit-btn"
+                                                onClick={() => editRow(idx)}
+                                            />
+                                        </span>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div >
+        </div>
     );
 };
