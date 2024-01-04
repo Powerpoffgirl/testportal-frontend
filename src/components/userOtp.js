a1f7e5ff8f4e532571ed8a5145b4bb1868eccef0
cyberne7work <cyberne7work@gmail.com>
2024-01-04 17:25:59 +0530

    user otp issue fixed

--- src/components/userOtp.js
+++ src/components/userOtp.js
@@ -7,7 +7,8 @@ import { LuRefreshCcw } from "react-icons/lu";
 import "./userLogin.css";
 import { Tooltip } from "antd";
 
-const UserOTP = () => {
+const UserOTP = () =>
+{
   const [otp, setOtp] = useState();
   const otpInputs = [];
   const [mobileNo, setMobileNo] = useState();
@@ -25,19 +26,22 @@ const UserOTP = () => {
 
   console.log("LOCATION STATE", location.state);
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     const contactNumber = localStorage.getItem("contactNumber");
     setDoctorName(localStorage.getItem("doctorName"));
     setMobileNo(contactNumber);
   }, []);
 
-  const SendOTP = async () => {
+  const SendOTP = async () =>
+  {
     const requestBody = {
       contactNumber: mobileNo,
     };
     const apiUrl = `${baseUrl}/api/v1/user/send_otp`;
 
-    try {
+    try
+    {
       // Send the POST request
       const response = await fetch(apiUrl, {
         method: "POST",
@@ -53,37 +57,45 @@ const UserOTP = () => {
 
       // Check the response status
       console.log("==========================DATA===================", data);
-      if (response.ok) {
+      if (response.ok)
+      {
         console.log("OTP sent successfully", data);
 
         setResendClicked(true);
         setSeconds(90);
         toast.success("Otp sent !!");
-      } else {
+      } else
+      {
         console.error("Error sending OTP:", data);
         toast.error("Error sending Otp");
       }
-    } catch (error) {
+    } catch (error)
+    {
       console.error("Error during the API call:", error);
     }
   };
 
-  const handleMobileNumberChange = (e) => {
+  const handleMobileNumberChange = (e) =>
+  {
     const { name, value } = e.target;
 
-    if (name === "otp") {
+    if (name === "otp")
+    {
       setOtp(value);
     }
   };
 
   console.log("otp output$$$$$$$$$$$", otp);
 
-  const verifyOTP = async () => {
-    if (otp?.length < 6) {
+  const verifyOTP = async () =>
+  {
+    if (otp?.length < 6)
+    {
       setOtperror("Please enter valid otp");
     }
 
-    try {
+    try
+    {
       const id = localStorage.getItem("userId");
 
       // const otpString = otp.join("");
@@ -96,51 +108,48 @@ const UserOTP = () => {
       });
 
       const data = await response.json();
-      if (data.success === true) {
+      if (data.success === true)
+      {
         localStorage.setItem("token", data?.data?.token);
         localStorage.setItem("pic", data?.data?.data?.userPic);
         console.log("token", data?.data?.token);
         console.log("======NEW USER=======", data?.data?.data?.newUser);
-<<<<<<< HEAD
-
-        if (data?.data?.data?.newUser) {
-=======
         localStorage.setItem("patientId", data?.patient?._id)
         if (data?.data?.data?.newUser)
         {
->>>>>>> 28618ce454dd733d98b516adda4c3ee84116c361
           navigate("/userprofile", { state: { user: user } });
-        } else if (doctorName) {
+        } else if (doctorName)
+        {
           navigate("/bookappointment", { state: { user: user } });
-<<<<<<< HEAD
-        } else {
-          navigate("/patientlistuser", { state: { user: user } });
-=======
         } else
         {
           navigate("/doctorlistuser", { state: { user: user } });
->>>>>>> 28618ce454dd733d98b516adda4c3ee84116c361
         }
       }
-      if (data.success === false) {
+      if (data.success === false)
+      {
         toast.error("OTP expired!");
       }
       console.log("DATA from response", data);
-    } catch (error) {
+    } catch (error)
+    {
       console.error("There was an error verifying the OTP:", error);
     }
   };
 
-  const handleInputChange = (e, index) => {
+  const handleInputChange = (e, index) =>
+  {
     const value = e.target.value;
 
-    if (isNaN(value)) {
+    if (isNaN(value))
+    {
       return; // Allow only numeric input
     }
 
     otp[index] = value;
 
-    if (index < MAX_LENGTH - 1 && value) {
+    if (index < MAX_LENGTH - 1 && value)
+    {
       otpInputs[index + 1].focus();
     }
 
@@ -151,12 +160,17 @@ const UserOTP = () => {
   console.log("INPUT OTP", otpInputs);
   console.log("Mobile No", mobileNo);
 
-  useEffect(() => {
-    if (resendClicked || firstTime) {
-      const intervalId = setInterval(() => {
-        if (seconds > 0) {
+  useEffect(() =>
+  {
+    if (resendClicked || firstTime)
+    {
+      const intervalId = setInterval(() =>
+      {
+        if (seconds > 0)
+        {
           setSeconds((prevSeconds) => prevSeconds - 1);
-        } else {
+        } else
+        {
           setFirstTime(false);
           setSeconds(90);
           setResendClicked(false);
@@ -166,7 +180,8 @@ const UserOTP = () => {
     }
   }, [seconds, resendClicked, firstTime]);
 
-  const formatTime = (time) => {
+  const formatTime = (time) =>
+  {
     const minutes = Math.floor(time / 60);
     const remainingSeconds = time % 60;
     return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
