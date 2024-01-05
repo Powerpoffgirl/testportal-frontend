1d6131d58424f6078433e4bf3e596cfb7cd27505
cyberne7work <cyberne7work@gmail.com>
2024-01-05 13:34:10 +0530

    appointment issue fixed

--- src/components/doctorList.js
+++ src/components/doctorList.js
@@ -935,9 +935,6 @@ export default function DoctorList({ searchTerm })
                             >
                               Book Appointment
                             </button>
-                            {/* <button className="text-white text-xs rounded-3xl px-3 py-1 " onClick={() => { showSlot() }} style={{ backgroundColor: ' #89CFF0' }}>
-                          Go Back
-                        </button> */}
                           </div>
                         )}
                       </div>
