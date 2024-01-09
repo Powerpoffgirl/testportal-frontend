b7cc62924c693b692182a05a70ee50c9002a41af
Anjali Patel <anjalipatel@Anjalis-MacBook-Air.local>
2024-01-09 14:38:14 +0530

    asd

--- src/components/appointmentList.js
+++ src/components/appointmentList.js
@@ -12,7 +12,6 @@ import { FaEdit } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import { Button, Popconfirm } from "antd";
 import UserContext from "./userContext";
-import close_button from "../assets/close_button.svg";
 
 const svg1 = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M17.7778 10C17.7778 7.83333 17.0231 5.99537 15.5139 4.48611C14.0046 2.97685 12.1667 2.22222 10 2.22222V0C11.3889 0 12.6898 0.263889 13.9028 0.791667C15.1157 1.31944 16.1713 2.03241 17.0694 2.93056C17.9676 3.8287 18.6806 4.88426 19.2083 6.09722C19.7361 7.31019 20 8.61111 20 10H17.7778ZM13.3333 10C13.3333 9.07407 13.0093 8.28704 12.3611 7.63889C11.713 6.99074 10.9259 6.66667 10 6.66667V4.44444C11.537 4.44444 12.8472 4.98611 13.9306 6.06944C15.0139 7.15278 15.5556 8.46296 15.5556 10H13.3333ZM18.8333 20C16.5185 20 14.2315 19.4954 11.9722 18.4861C9.71296 17.4769 7.65741 16.0463 5.80556 14.1944C3.9537 12.3426 2.52315 10.287 1.51389 8.02778C0.50463 5.76852 0 3.48148 0 1.16667C0 0.833333 0.111111 0.555556 0.333333 0.333333C0.555556 0.111111 0.833333 0 1.16667 0H5.66667C5.92593 0 6.15741 0.087963 6.36111 0.263889C6.56482 0.439815 6.68519 0.648148 6.72222 0.888889L7.44444 4.77778C7.48148 5.07407 7.47222 5.32407 7.41667 5.52778C7.36111 5.73148 7.25926 5.90741 7.11111 6.05556L4.41667 8.77778C4.78704 9.46296 5.22685 10.125 5.73611 10.7639C6.24537 11.4028 6.80556 12.0185 7.41667 12.6111C7.99074 13.1852 8.59259 13.7176 9.22222 14.2083C9.85185 14.6991 10.5185 15.1481 11.2222 15.5556L13.8333 12.9444C14 12.7778 14.2176 12.6528 14.4861 12.5694C14.7546 12.4861 15.0185 12.463 15.2778 12.5L19.1111 13.2778C19.3704 13.3519 19.5833 13.4861 19.75 13.6806C19.9167 13.875 20 14.0926 20 14.3333V18.8333C20 19.1667 19.8889 19.4444 19.6667 19.6667C19.4444 19.8889 19.1667 20 18.8333 20ZM3.36111 6.66667L5.19444 4.83333L4.72222 2.22222H2.25C2.34259 2.98148 2.47222 3.73148 2.63889 4.47222C2.80556 5.21296 3.0463 5.94444 3.36111 6.66667ZM13.3056 16.6111C14.0278 16.9259 14.7639 17.1759 15.5139 17.3611C16.2639 17.5463 17.0185 17.6667 17.7778 17.7222V15.2778L15.1667 14.75L13.3056 16.6111Z" fill="#89CFF0"/>
@@ -34,12 +33,8 @@ const svg5 = `<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns=
 </svg>`;
 
 export default function AppointmentList({ searchTerm }) {
-<<<<<<< HEAD
   const { updateUser, updateUserEmail, updateUserimage } =
     useContext(UserContext);
-=======
-  const { updateUser, updateUserEmail, updateUserimage } = useContext(UserContext);
->>>>>>> 4c6909ebb0105d33109af6d8f1e649ca00d12de0
   let isTab = useMediaQuery({ query: "(max-width: 768px)" });
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const [patientsList, setPatientsList] = useState([]);
@@ -49,7 +44,7 @@ export default function AppointmentList({ searchTerm }) {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+  const onCloseModall = () => setOpen(false);
   const [modalOpen, setModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState("");
   const [diseasesinfo, setDiseasesinfo] = useState("");
@@ -63,17 +58,10 @@ export default function AppointmentList({ searchTerm }) {
     appointmentList,
   ]);
 
-<<<<<<< HEAD
-  // const onCloseModal = () => {
-  //   setModalOpen(false);
-  //   setModalContent("");
-  // };
-=======
   const onCloseModal = () => {
     setModalOpen(false);
     setModalContent("");
   };
->>>>>>> 4c6909ebb0105d33109af6d8f1e649ca00d12de0
   useEffect(() => {
     const fetchUserDetails = async () => {
       try {
@@ -325,40 +313,28 @@ export default function AppointmentList({ searchTerm }) {
 
       <Modal
         open={open}
-        onClose={onCloseModal}
+        onClose={onCloseModall}
         center
         patient={selectedAppointment}
         styles={{
           modal: {
             width: isTab ? "80%" : "70%",
-            backgroundColor: "#E3F6FF",
+            backgroundColor: "#fff",
             alignContent: "center",
-            borderRadius: "23px",
-          },
-          closeButton: {
-            display: "none",
           },
         }}
       >
-        <div class="flex flex-row-reverse md:-mb-14  -mb-14 z-50">
-          <button className="z-50" onClick={onCloseModal}>
-            <img src={close_button} className="w-8 mb-1"></img>
-          </button>
-        </div>
         <div className="flex flex-col bg-customRedp-2 w-[100%] md:w-[100%]  mt-[2%]">
           <div className="flex flex-row w-[100%] justify-center ">
             <AccountCircleIcon
-              // className="shadow-xl "
               style={{
                 fontSize: "90px",
-                color: "white",
-                // backgroundColor: "red",
+                color: "#B1DAED",
                 borderRadius: "50%",
                 height: isTab ? "40px" : "123px",
                 width: isTab ? "40px" : "123px",
                 marginRight: "70px",
-                padding: "0px",
-                // boxShadow: "0px 0px 5px 2px rgba(0, 0, 0, 0.5)",
+                boxShadow: "inset 0 0 0 2px #76767",
               }}
             />
           </div>
