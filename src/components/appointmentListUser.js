12d98ebd5f396d89d499d8d46fecd4143fb37ae7
Anjali Patel <anjalipatel@Anjalis-MacBook-Air.local>
2024-01-15 11:51:01 +0530

    sdfg

--- src/components/appointmentListUser.js
+++ src/components/appointmentListUser.js
@@ -10,8 +10,7 @@ import { Button, Popconfirm } from "antd";
 import close_button from "../assets/close_button.svg";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-export default function AppointmentListUser({ searchTerm })
-{
+export default function AppointmentListUser({ searchTerm }) {
   let isTab = useMediaQuery({ query: "(max-width: 767px)" });
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const [appointmentList, setAppointmentList] = useState([]);
@@ -25,15 +24,11 @@ export default function AppointmentListUser({ searchTerm })
     appointmentList,
   ]);
 
-  useEffect(() =>
-  {
-    const fetchPatientDetails = async () =>
-    {
-      try
-      {
+  useEffect(() => {
+    const fetchPatientDetails = async () => {
+      try {
         const token = localStorage.getItem("token");
-        if (!token)
-        {
+        if (!token) {
           console.error("No token found in local storage");
           return;
         }
@@ -50,20 +45,17 @@ export default function AppointmentListUser({ searchTerm })
         const data = await response.json();
         console.log("DATA from response", data);
         setAppointmentList(data?.data);
-      } catch (error)
-      {
+      } catch (error) {
         console.error("There was an error verifying the OTP:", error);
       }
     };
     fetchPatientDetails();
   }, []);
 
-  useEffect(() =>
-  {
+  useEffect(() => {
     let matchedDoctors = [];
 
-    if (appointmentList?.length > 0 && searchTerm)
-    {
+    if (appointmentList?.length > 0 && searchTerm) {
       const lowerCaseSearchTerm = searchTerm.toLowerCase().trim();
 
       matchedDoctors = appointmentList.filter(
@@ -75,8 +67,7 @@ export default function AppointmentListUser({ searchTerm })
             .toLowerCase()
             .includes(lowerCaseSearchTerm)
       );
-    } else
-    {
+    } else {
       matchedDoctors = appointmentList;
     }
 
@@ -88,15 +79,12 @@ export default function AppointmentListUser({ searchTerm })
     appointmentDate,
     appointmentTime,
     doctorId
-  ) =>
-  {
+  ) => {
     localStorage.setItem("appointmentId", appointmentId);
 
-    try
-    {
+    try {
       const token = localStorage.getItem("token");
-      if (!token)
-      {
+      if (!token) {
         console.error("No token found in local storage");
         return;
       }
@@ -120,14 +108,12 @@ export default function AppointmentListUser({ searchTerm })
 
       const data = await response.json();
 
-      if (response.ok)
-      {
+      if (response.ok) {
         console.log("Appointment slot deleted successfully", data);
 
         navigate("/editappointment");
       }
-    } catch (error)
-    {
+    } catch (error) {
       console.error("There was an error deleting the Appointment:", error);
     }
   };
@@ -137,13 +123,10 @@ export default function AppointmentListUser({ searchTerm })
     appointmentDate,
     appointmentTime,
     doctorId
-  ) =>
-  {
-    try
-    {
+  ) => {
+    try {
       const token = localStorage.getItem("token");
-      if (!token)
-      {
+      if (!token) {
         console.error("No token found in local storage");
         return;
       }
@@ -160,8 +143,7 @@ export default function AppointmentListUser({ searchTerm })
 
       const data = await response.json();
 
-      if (response.ok)
-      {
+      if (response.ok) {
         console.log("Appointment deleted successfully", data);
         toast.success("Appointment Deleted!");
         // toast.success("Appointment Deleted")
@@ -192,29 +174,24 @@ export default function AppointmentListUser({ searchTerm })
 
         const data1 = await response1.json();
 
-        if (response1.ok)
-        {
+        if (response1.ok) {
           console.log("Appointment slot deleted successfully", data);
         }
-      } else
-      {
+      } else {
         console.error("Failed to delete the doctor", data?.message);
       }
-    } catch (error)
-    {
+    } catch (error) {
       console.error("There was an error deleting the Appointment:", error);
     }
   };
 
-  function formatDate(dateString)
-  {
+  function formatDate(dateString) {
     const parts = dateString.split("-");
     return `${parts[2]}.${parts[1]}.${parts[0]}`;
   }
   console.log("APPOINTMENT LISTS", appointmentList, selectedAppointment);
 
-  const findSelectedDoctor = async (appointmentId) =>
-  {
+  const findSelectedDoctor = async (appointmentId) => {
     console.log("appointmentId########################", appointmentId);
     // Assuming doctorsList is an array of doctor objects and each doctor has an _id field.
     const appointment = appointmentList?.find(
@@ -372,16 +349,16 @@ export default function AppointmentListUser({ searchTerm })
           >
             {selectedAppointment?.patientId?.address?.houseNo
               ? selectedAppointment?.patientId?.address?.houseNo +
-              " " +
-              selectedAppointment?.patientId?.address?.block +
-              " " +
-              selectedAppointment?.patientId?.address?.area +
-              ", " +
-              selectedAppointment?.patientId?.address?.district +
-              ", " +
-              selectedAppointment?.patientId?.address?.state +
-              " " +
-              selectedAppointment?.patientId?.address?.pinCode
+                " " +
+                selectedAppointment?.patientId?.address?.block +
+                " " +
+                selectedAppointment?.patientId?.address?.area +
+                ", " +
+                selectedAppointment?.patientId?.address?.district +
+                ", " +
+                selectedAppointment?.patientId?.address?.state +
+                " " +
+                selectedAppointment?.patientId?.address?.pinCode
               : " "}
           </text>
         </div>
@@ -470,9 +447,9 @@ export default function AppointmentListUser({ searchTerm })
                           </p>
                           {appointment?.appointmentDate?.date
                             ? appointment?.appointmentDate?.date
-                              .split("-")
-                              .reverse()
-                              .join("-")
+                                .split("-")
+                                .reverse()
+                                .join("-")
                             : "No Date"}
                         </h1>
                       </div>
