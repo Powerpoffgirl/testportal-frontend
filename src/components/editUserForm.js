31988ca783bde6e6fbc309ae7561898a8b4f6f6f
ramkumar21234 <126386524+RaM21234@users.noreply.github.com>
2024-01-15 16:29:11 +0530

    co

--- src/components/editUserForm.js
+++ src/components/editUserForm.js
@@ -521,7 +521,7 @@ export default function EditUserForm() {
           (appointment?.appointmentDate?.date >=
             appointmentDetails?.appointmentDate?.date ||
             appointment?.appointmentDate?.date <
-              appointmentDetails?.appointmentDate?.date)
+            appointmentDetails?.appointmentDate?.date)
         ) {
           console.log(
             "IDS ARE MATCHING",
@@ -771,7 +771,7 @@ export default function EditUserForm() {
                       Age
                     </label>
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <input
                         type="text"
                         id="age"
@@ -803,7 +803,7 @@ export default function EditUserForm() {
                       Age Type
                     </label>
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <Select
                         className="border rounded-lg h-11"
                         popupClassName="no-border-dropdown-menu"
@@ -861,7 +861,7 @@ export default function EditUserForm() {
                       Gender
                     </label>
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <Select
                         className="border rounded-lg h-11"
                         popupClassName="no-border-dropdown-menu"
@@ -1104,114 +1104,118 @@ export default function EditUserForm() {
             </label>
             <div className="p-3 pb-5 border shadow-lg rounded-md">
               <div className="flex flex-col ">
-                <div className="flex flex-row">
-                  <div className="px-2 w-1/4  mt-3">
-                    {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
-                      <input
-                        type="text"
-                        placeholder="House No."
-                        id="houseNo"
-                        name="houseNo"
-                        onChange={handleChange}
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    ) : (
-                      <input
-                        type="text"
-                        placeholder="House No."
-                        id="houseNo"
-                        name="houseNo"
-                        value={patientDetails?.address?.houseNo}
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    )}
+                <div className="flex lg:flex-row flex-col">
+                  <div class="flex flex-row ">
+                    <div className="px-2 lg:w-1/2  mt-3">
+                      {patientsList?.length === 0 ||
+                        userDetails?.newUser === true ? (
+                        <input
+                          type="text"
+                          placeholder="House No."
+                          id="houseNo"
+                          name="houseNo"
+                          onChange={handleChange}
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      ) : (
+                        <input
+                          type="text"
+                          placeholder="House No."
+                          id="houseNo"
+                          name="houseNo"
+                          value={patientDetails?.address?.houseNo}
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      )}
+                    </div >
+                    <div className="px-2 lg:w-1/2 mt-3">
+                      {patientsList?.length === 0 ||
+                        userDetails?.newUser === true ? (
+                        <input
+                          type="text"
+                          id="floor"
+                          name="floor"
+                          onChange={handleChange}
+                          placeholder="Floor"
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      ) : (
+                        <input
+                          type="text"
+                          id="floor"
+                          name="floor"
+                          value={patientDetails?.address?.floor}
+                          placeholder="Floor"
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      )}
+                    </div>
                   </div>
-                  <div className="px-2 w-1/4 mt-3">
-                    {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
-                      <input
-                        type="text"
-                        id="floor"
-                        name="floor"
-                        onChange={handleChange}
-                        placeholder="Floor"
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    ) : (
-                      <input
-                        type="text"
-                        id="floor"
-                        name="floor"
-                        value={patientDetails?.address?.floor}
-                        placeholder="Floor"
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    )}
-                  </div>
-                  <div className="px-2 w-1/4 mt-3">
-                    {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
-                      <input
-                        type="text"
-                        id="block"
-                        name="block"
-                        onChange={handleChange}
-                        placeholder="Block"
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    ) : (
-                      <input
-                        type="text"
-                        id="block"
-                        name="block"
-                        value={patientDetails?.address?.block}
-                        placeholder="Block"
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    )}
+                  <div class="flex flex-row">
+                    <div className="px-2 lg:w-1/2 mt-3">
+                      {patientsList?.length === 0 ||
+                        userDetails?.newUser === true ? (
+                        <input
+                          type="text"
+                          id="block"
+                          name="block"
+                          onChange={handleChange}
+                          placeholder="Block"
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      ) : (
+                        <input
+                          type="text"
+                          id="block"
+                          name="block"
+                          value={patientDetails?.address?.block}
+                          placeholder="Block"
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      )}
 
-                    {errors.block && (
-                      <p className="text-red-500">{errors.block}</p>
-                    )}
-                  </div>
-                  <div className="px-2 w-1/4 mt-3">
-                    {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
-                      <input
-                        type="text"
-                        id="pinCode"
-                        name="pinCode"
-                        onChange={handleChange}
-                        placeholder="Pin Code"
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                        onInput={(e) => {
-                          e.target.value = e.target.value.replace(
-                            /[^0-9]/g,
-                            ""
-                          );
-                        }}
-                      />
-                    ) : (
-                      <input
-                        type="text"
-                        id="pinCode"
-                        name="pinCode"
-                        value={patientDetails?.address?.pinCode}
-                        placeholder="Pin Code"
-                        className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                      />
-                    )}
+                      {errors.block && (
+                        <p className="text-red-500">{errors.block}</p>
+                      )}
+                    </div>
+                    <div className="px-2 lg:w-1/2 mt-3">
+                      {patientsList?.length === 0 ||
+                        userDetails?.newUser === true ? (
+                        <input
+                          type="text"
+                          id="pinCode"
+                          name="pinCode"
+                          onChange={handleChange}
+                          placeholder="Pin Code"
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                          onInput={(e) => {
+                            e.target.value = e.target.value.replace(
+                              /[^0-9]/g,
+                              ""
+                            );
+                          }}
+                        />
+                      ) : (
+                        <input
+                          type="text"
+                          id="pinCode"
+                          name="pinCode"
+                          value={patientDetails?.address?.pinCode}
+                          placeholder="Pin Code"
+                          className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                        />
+                      )}
 
-                    {pinCodeError && (
-                      <p className="text-red-500">{pinCodeError}</p>
-                    )}
+                      {pinCodeError && (
+                        <p className="text-red-500">{pinCodeError}</p>
+                      )}
+                    </div>
                   </div>
                 </div>
                 {/* ----------------------------area/landmark---------------------------- */}
                 <div className="px-2 w-full mt-3 ">
                   {patientsList?.length === 0 ||
-                  userDetails?.newUser === true ? (
+                    userDetails?.newUser === true ? (
                     <input
                       type="text"
                       id="area"
@@ -1237,7 +1241,7 @@ export default function EditUserForm() {
                 <div className="flex flex-row">
                   <div className="px-2 w-1/2 mt-3">
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <input
                         type="text"
                         id="district"
@@ -1264,7 +1268,7 @@ export default function EditUserForm() {
 
                   <div className="px-2 w-1/2 mt-3">
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <input
                         type="text"
                         id="state"
