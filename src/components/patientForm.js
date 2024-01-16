11938343739689fec1304c2a3049fa1c402171ba
cyberne7work <cyberne7work@gmail.com>
2024-01-16 13:20:55 +0530

    co

--- src/components/patientForm.js
+++ src/components/patientForm.js
@@ -13,23 +13,13 @@ import { HiOutlineUserAdd } from "react-icons/hi";
 import { FaAngleDown } from "react-icons/fa";
 import { Select } from "antd";
 
-const svg1 = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M17.7778 10C17.7778 7.83333 17.0231 5.99537 15.5139 4.48611C14.0046 2.97685 12.1667 2.22222 10 2.22222V0C11.3889 0 12.6898 0.263889 13.9028 0.791667C15.1157 1.31944 16.1713 2.03241 17.0694 2.93056C17.9676 3.8287 18.6806 4.88426 19.2083 6.09722C19.7361 7.31019 20 8.61111 20 10H17.7778ZM13.3333 10C13.3333 9.07407 13.0093 8.28704 12.3611 7.63889C11.713 6.99074 10.9259 6.66667 10 6.66667V4.44444C11.537 4.44444 12.8472 4.98611 13.9306 6.06944C15.0139 7.15278 15.5556 8.46296 15.5556 10H13.3333ZM18.8333 20C16.5185 20 14.2315 19.4954 11.9722 18.4861C9.71296 17.4769 7.65741 16.0463 5.80556 14.1944C3.9537 12.3426 2.52315 10.287 1.51389 8.02778C0.50463 5.76852 0 3.48148 0 1.16667C0 0.833333 0.111111 0.555556 0.333333 0.333333C0.555556 0.111111 0.833333 0 1.16667 0H5.66667C5.92593 0 6.15741 0.087963 6.36111 0.263889C6.56482 0.439815 6.68519 0.648148 6.72222 0.888889L7.44444 4.77778C7.48148 5.07407 7.47222 5.32407 7.41667 5.52778C7.36111 5.73148 7.25926 5.90741 7.11111 6.05556L4.41667 8.77778C4.78704 9.46296 5.22685 10.125 5.73611 10.7639C6.24537 11.4028 6.80556 12.0185 7.41667 12.6111C7.99074 13.1852 8.59259 13.7176 9.22222 14.2083C9.85185 14.6991 10.5185 15.1481 11.2222 15.5556L13.8333 12.9444C14 12.7778 14.2176 12.6528 14.4861 12.5694C14.7546 12.4861 15.0185 12.463 15.2778 12.5L19.1111 13.2778C19.3704 13.3519 19.5833 13.4861 19.75 13.6806C19.9167 13.875 20 14.0926 20 14.3333V18.8333C20 19.1667 19.8889 19.4444 19.6667 19.6667C19.4444 19.8889 19.1667 20 18.8333 20ZM3.36111 6.66667L5.19444 4.83333L4.72222 2.22222H2.25C2.34259 2.98148 2.47222 3.73148 2.63889 4.47222C2.80556 5.21296 3.0463 5.94444 3.36111 6.66667ZM13.3056 16.6111C14.0278 16.9259 14.7639 17.1759 15.5139 17.3611C16.2639 17.5463 17.0185 17.6667 17.7778 17.7222V15.2778L15.1667 14.75L13.3056 16.6111Z" fill="#89CFF0"/>
-</svg>`;
-const svg2 = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 12H12V10H4V12ZM4 9H16V7H4V9ZM4 6H16V4H4V6ZM0 20V2C0 1.45 0.195833 0.979167 0.5875 0.5875C0.979167 0.195833 1.45 0 2 0H18C18.55 0 19.0208 0.195833 19.4125 0.5875C19.8042 0.979167 20 1.45 20 2V14C20 14.55 19.8042 15.0208 19.4125 15.4125C19.0208 15.8042 18.55 16 18 16H4L0 20ZM3.15 14H18V2H2V15.125L3.15 14Z" fill="#89CFF0"/>
-</svg>`;
 
-const svg3 = `<svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M12.5 0L15.3064 8.63729H24.3882L17.0409 13.9754L19.8473 22.6127L12.5 17.2746L5.15268 22.6127L7.95911 13.9754L0.611794 8.63729H9.69357L12.5 0Z" fill="#FFF500"/>
-</svg>`;
-
-export default function PatientForm() {
+export default function PatientForm()
+{
   let isTab = useMediaQuery({ query: "(max-width: 768px)" });
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const [selectedDoctor, setselectedDoctor] = useState();
   const [isEditing, setIsEditing] = useState(false);
-
   const [open1, setOpen1] = useState(false);
   const onOpenModal = () => setOpen1(true);
   const onCloseModal = () => setOpen1(false);
@@ -37,7 +27,6 @@ export default function PatientForm() {
   const [name, setName] = useState("");
   const [nameError, setNameError] = useState("");
   const [patientsList, setPatientsList] = useState([]);
-
   const [age, setAge] = useState("");
   const [ageError, setAgeError] = useState("");
   const [bodyWeight, setBodyWeight] = useState("");
@@ -71,15 +60,18 @@ export default function PatientForm() {
   const [doctorImage, setDoctorImage] = useState();
   const [mobileNumberError, setmobileNumberError] = useState("");
 
-  const handleFileSelect = async (event) => {
+  const handleFileSelect = async (event) =>
+  {
     const file = event.target.files[0];
-    if (file) {
+    if (file)
+    {
       const token = localStorage.getItem("token");
       const formData = new FormData();
       formData.append("userPic", file);
 
       console.log("FORM DATA", formData);
-      try {
+      try
+      {
         const response = await fetch(`${baseUrl}/api/v1/upload_image`, {
           method: "POST",
           headers: {
@@ -88,7 +80,8 @@ export default function PatientForm() {
           body: formData,
         });
 
-        if (!response.ok) {
+        if (!response.ok)
+        {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
@@ -101,7 +94,8 @@ export default function PatientForm() {
         // Reset the file input
         setSelectedFile(null);
         fileInputRef.current.value = "";
-      } catch (error) {
+      } catch (error)
+      {
         console.error("Error uploading image:", error);
         toast.error("Error uploading image. Please try again.");
       }
@@ -127,41 +121,52 @@ export default function PatientForm() {
     patientPic: "",
   });
 
-  const handleClick = (event) => {
+  const handleClick = (event) =>
+  {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleClose = () =>
+  {
     setAnchorEl(null);
   };
 
-  const handleChange1 = (e) => {
+  const handleChange1 = (e) =>
+  {
     setPatientDetails((prevPatientDetails) => ({
       ...prevPatientDetails,
       gender: e,
     }));
   };
-  const handleChange2 = (e) => {
+  const handleChange2 = (e) =>
+  {
     setPatientDetails((prevPatientDetails) => ({
       ...prevPatientDetails,
       ageType: e,
     }));
   };
 
-  const handleChange = (e) => {
+  const handleChange = (e) =>
+  {
     const { name, value } = e.target;
-    if (name === "pinCode") {
-      if (/^\d{6}$/.test(value) && !/[A-Za-z]/.test(value)) {
+    if (name === "pinCode")
+    {
+      if (/^\d{6}$/.test(value) && !/[A-Za-z]/.test(value))
+      {
         setPinCodeError("");
-      } else {
+      } else
+      {
         setPinCodeError("Please enter a valid Pincode");
       }
     }
 
-    if (name === "contactNumber") {
-      if (/^\d{10}$/.test(value) && !/[A-Za-z]/.test(value)) {
+    if (name === "contactNumber")
+    {
+      if (/^\d{10}$/.test(value) && !/[A-Za-z]/.test(value))
+      {
         setmobileNumberError("");
-      } else {
+      } else
+      {
         setmobileNumberError("Please enter a valid Number");
       }
     }
@@ -179,11 +184,11 @@ export default function PatientForm() {
         "state",
       ].includes(name)
         ? {
-            address: {
-              ...prevPatientDetails.address,
-              [name]: value,
-            },
-          }
+          address: {
+            ...prevPatientDetails.address,
+            [name]: value,
+          },
+        }
         : { [name]: value }),
     }));
 
@@ -197,7 +202,8 @@ export default function PatientForm() {
         "district",
         "state",
       ].includes(name)
-    ) {
+    )
+    {
       setPatientDetails((prevPatientDetails) => ({
         ...prevPatientDetails,
         address: {
@@ -205,7 +211,8 @@ export default function PatientForm() {
           [name]: value,
         },
       }));
-    } else {
+    } else
+    {
       setPatientDetails((prevPatientDetails) => ({
         ...prevPatientDetails,
         [name]: value,
@@ -225,7 +232,8 @@ export default function PatientForm() {
     { label: "Other", value: "Other" },
   ];
 
-  const handleRegister = async (e) => {
+  const handleRegister = async (e) =>
+  {
     e.preventDefault();
 
     const newPatientDetails = {
@@ -247,29 +255,41 @@ export default function PatientForm() {
       patientPic: userImage,
     };
 
-    if (newPatientDetails.gender === "") {
+    if (newPatientDetails.gender === "")
+    {
       toast.error("Please write gender");
-    } else if (newPatientDetails.age === "") {
+    } else if (newPatientDetails.age === "")
+    {
       toast.error("Please write age");
-    } else if (newPatientDetails.ageType === "") {
+    } else if (newPatientDetails.ageType === "")
+    {
       toast.error("Please write Age Type");
-    } else if (newPatientDetails.bodyWeight === "") {
+    } else if (newPatientDetails.bodyWeight === "")
+    {
       toast.error("Please write Body Weight");
-    } else if (newPatientDetails.name === "") {
+    } else if (newPatientDetails.name === "")
+    {
       toast.error("Please write name");
-    } else if (newPatientDetails.contactNumber === "") {
+    } else if (newPatientDetails.contactNumber === "")
+    {
       toast.error("Please write Contact Number");
-    } else if (!newPatientDetails.address?.pinCode) {
+    } else if (!newPatientDetails.address?.pinCode)
+    {
       toast.error("Please write Pincode");
-    } else if (!/^\d{6}$/.test(newPatientDetails.address?.pinCode)) {
+    } else if (!/^\d{6}$/.test(newPatientDetails.address?.pinCode))
+    {
       toast.error("Please enter a valid 6-digit PIN code");
-    } else if (newPatientDetails.address?.district === "") {
+    } else if (newPatientDetails.address?.district === "")
+    {
       toast.error("Please write district");
-    } else if (newPatientDetails.address?.state === "") {
+    } else if (newPatientDetails.address?.state === "")
+    {
       toast.error("Please write state");
-    } else {
+    } else
+    {
       const token = localStorage.getItem("token");
-      if (!token) {
+      if (!token)
+      {
         console.error("No token found in local storage");
         localStorage.clear();
         navigate(`/userlogin`);
@@ -283,7 +303,8 @@ export default function PatientForm() {
         body: JSON.stringify(newPatientDetails),
       });
       const data = await response.json();
-      if (data.success === true) {
+      if (data.success === true)
+      {
         onOpenModal();
         localStorage.setItem("patientId", data.data._id);
         toast.success("Member details updated!");
@@ -382,7 +403,8 @@ export default function PatientForm() {
                       backgroundColor: "#89CFF0",
                       color: isHovered ? "red" : "white",
                     }}
-                    onClick={() => {
+                    onClick={() =>
+                    {
                       handleClose();
                     }}
                     onMouseEnter={() => setIsHovered(true)}
@@ -552,7 +574,8 @@ export default function PatientForm() {
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               required
               onChange={handleChange}
-              onInput={(e) => {
+              onInput={(e) =>
+              {
                 e.target.value = e.target.value.replace(/[^0-9]/g, "");
               }}
               className="block  w-full placeholder-gray-400  rounded-lg border  bg-white px-5 py-2.5 text-gray-900  focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
@@ -575,7 +598,7 @@ export default function PatientForm() {
                   <div class="flex flex-row ">
                     <div className="px-2 lg:w-1/2  mt-3">
                       {patientsList?.length === 0 ||
-                      userDetails?.newUser === true ? (
+                        userDetails?.newUser === true ? (
                         <input
                           type="text"
                           placeholder="House No."
@@ -597,7 +620,7 @@ export default function PatientForm() {
                     </div>
                     <div className="px-2 lg:w-1/2 mt-3">
                       {patientsList?.length === 0 ||
-                      userDetails?.newUser === true ? (
+                        userDetails?.newUser === true ? (
                         <input
                           type="text"
                           id="floor"
@@ -621,7 +644,7 @@ export default function PatientForm() {
                   <div class="flex flex-row">
                     <div className="px-2 lg:w-1/2 mt-3">
                       {patientsList?.length === 0 ||
-                      userDetails?.newUser === true ? (
+                        userDetails?.newUser === true ? (
                         <input
                           type="text"
                           id="block"
@@ -647,7 +670,7 @@ export default function PatientForm() {
                     </div>
                     <div className="px-2 lg:w-1/2 mt-3">
                       {patientsList?.length === 0 ||
-                      userDetails?.newUser === true ? (
+                        userDetails?.newUser === true ? (
                         <input
                           type="text"
                           id="pinCode"
@@ -655,7 +678,8 @@ export default function PatientForm() {
                           onChange={handleChange}
                           placeholder="Pin Code"
                           className="block w-full rounded-lg border  bg-[#EAEAEA] placeholder-gray-500 font-medium px-5 py-2.5 text-gray-700 focus:border-[#89CFF0] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
-                          onInput={(e) => {
+                          onInput={(e) =>
+                          {
                             e.target.value = e.target.value.replace(
                               /[^0-9]/g,
                               ""
@@ -682,7 +706,7 @@ export default function PatientForm() {
                 {/* ----------------------------area/landmark---------------------------- */}
                 <div className="px-2 w-full mt-3 ">
                   {patientsList?.length === 0 ||
-                  userDetails?.newUser === true ? (
+                    userDetails?.newUser === true ? (
                     <input
                       type="text"
                       id="area"
@@ -708,7 +732,7 @@ export default function PatientForm() {
                 <div className="flex flex-row">
                   <div className="px-2 w-1/2 mt-3">
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <input
                         type="text"
                         id="district"
@@ -735,7 +759,7 @@ export default function PatientForm() {
 
                   <div className="px-2 w-1/2 mt-3">
                     {patientsList?.length === 0 ||
-                    userDetails?.newUser === true ? (
+                      userDetails?.newUser === true ? (
                       <input
                         type="text"
                         id="state"
