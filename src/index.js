f1b8c412463991ac241d6f37c31648bcf1c4bd90
cyberne7work <cyberne7work@gmail.com>
2024-09-12 15:45:43 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import UserProvider from './components/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
 
